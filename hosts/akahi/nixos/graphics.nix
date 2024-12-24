02fc5ad1397b754490409e3c0b713484e54aa0d9
merrkry <merrkry@tsubasa.moe>
2024-12-24 15:06:15 +0100

    akahi: refactor vfio module loading
    hot switch is a lie!

--- hosts/akahi/nixos/graphics.nix
+++ hosts/akahi/nixos/graphics.nix
@@ -5,46 +5,55 @@
   ...
 }:
 {
-  environment.sessionVariables = {
-    __EGL_VENDOR_LIBRARY_FILENAMES = "/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json";
-    VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json";
-    __GLX_VENDOR_LIBRARY_NAME = "mesa";
+  config = lib.mkMerge [
+    {
+      environment.sessionVariables = {
+        __EGL_VENDOR_LIBRARY_FILENAMES = "/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json";
+        VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json";
+        __GLX_VENDOR_LIBRARY_NAME = "mesa";
 
-    # Intel GPU Vulkan decode, see https://github.com/mpv-player/mpv/discussions/13909
-    ANV_VIDEO_DECODE = "1";
-  };
+        # Intel GPU Vulkan decode, see https://github.com/mpv-player/mpv/discussions/13909
+        ANV_VIDEO_DECODE = "1";
+      };
+    }
+    (lib.mkIf (config.specialisation != { }) {
+      boot.extraModprobeConfig = ''
+        options nvidia NVreg_UsePageAttributeTable=1 NVreg_InitializeSystemMemoryAllocations=0
+      '';
 
-  environment.systemPackages = [
-    # __NV_PRIME_RENDER_OFFLOAD_PROVIDER may be useful for multi-screen
-    # https://download.nvidia.com/XFree86/Linux-x86_64/435.17/README/primerenderoffload.html
-    (pkgs.writeShellScriptBin "prime-run" ''
-      export __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json
-      export VK_DRIVER_FILES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json
-      export __NV_PRIME_RENDER_OFFLOAD=1
-      export __GLX_VENDOR_LIBRARY_NAME=nvidia
-      export __VK_LAYER_NV_optimus=NVIDIA_only
-      exec "$@"
-    '')
+      environment.systemPackages = [
+        # __NV_PRIME_RENDER_OFFLOAD_PROVIDER may be useful for multi-screen
+        # https://download.nvidia.com/XFree86/Linux-x86_64/435.17/README/primerenderoffload.html
+        (pkgs.writeShellScriptBin "prime-run" ''
+          export __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json
+          export VK_DRIVER_FILES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json
+          export __NV_PRIME_RENDER_OFFLOAD=1
+          export __GLX_VENDOR_LIBRARY_NAME=nvidia
+          export __VK_LAYER_NV_optimus=NVIDIA_only
+          exec "$@"
+        '')
+      ];
+
+      hardware = {
+        nvidia = {
+          modesetting.enable = true;
+          powerManagement.enable = true;
+          powerManagement.finegrained = true;
+          open = true;
+          nvidiaSettings = false;
+          nvidiaPersistenced = true;
+          dynamicBoost.enable = true;
+          package = config.boot.kernelPackages.nvidiaPackages.latest;
+          prime = {
+            intelBusId = "PCI:0:2:0";
+            nvidiaBusId = "PCI:1:0:0";
+            offload.enable = true;
+          };
+        };
+        nvidia-container-toolkit.enable = true;
+      };
+
+      services.xserver.videoDrivers = [ "nvidia" ];
+    })
   ];
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = true;
-    open = true;
-    nvidiaSettings = false;
-    nvidiaPersistenced = true;
-    dynamicBoost.enable = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
-    prime = {
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-      offload.enable = true;
-    };
-  };
-  hardware.nvidia-container-toolkit.enable = true;
-  boot.extraModprobeConfig = ''
-    options nvidia NVreg_UsePageAttributeTable=1 NVreg_InitializeSystemMemoryAllocations=0
-  '';
 }
--- hosts/akahi/nixos/virtualisation.nix
+++ hosts/akahi/nixos/virtualisation.nix
@@ -4,64 +4,41 @@
   pkgs,
   ...
 }:
-let
-  dgpu = {
-    bus = "0000:01:00.0";
-    info = "10de 249d";
-  };
-  # libvirt can handle snd_hda_intel automatically
-  # audio = {
-  #   bus = "0000:01:00.1";
-  #   info = "10de 228b";
-  # };
-
-  # modeset and fbdev is now enabled by default
-  # other options for nvidia drivers are defined in boot.extraModprobeConfig
-  switch-vfio = pkgs.writeShellScriptBin "switch-vfio" ''
-    if [[ $(whoami) != "root" ]]; then
-        exit 1
-    fi
-
-    get_vd() {
-        return "$(cat /sys/bus/pci/devices/$1/vendor) $(cat /sys/bus/pci/devices/$1/device)"
-    }
-
-    services=("nvidia-container-toolkit-cdi-generator.service" "nvidia-persistenced.service" "nvidia-powerd")
-
-    if [ "$1" == "bind" ]; then
-
-        for service in "''${services[@]}"; do
-            systemctl stop "$service"
-        done
-        # sometimes it will stuck here
-        echo "${dgpu.bus}" > /sys/bus/pci/devices/${dgpu.bus}/driver/unbind &>/dev/null
-        rmmod nvidia_uvm
-        rmmod nvidia_drm
-        rmmod nvidia_modeset
-        rmmod nvidia
-        modprobe vfio
-        modprobe vfio_pci_core
-        modprobe vfio_pci
-        modprobe vfio_iommu_type1
-        echo "${dgpu.info}" > /sys/bus/pci/drivers/vfio-pci/new_id
-
-    elif [ "$1" == "unbind" ]; then
-
-        echo "${dgpu.info}" > /sys/bus/pci/drivers/vfio-pci/remove_id
-        rmmod vfio_iommu_type1
-        rmmod vfio_pci
-        rmmod vfio_pci_core
-        rmmod vfio
-        echo "1" > /sys/bus/pci/devices/${dgpu.bus}/remove
-        echo 1 > /sys/bus/pci/rescan
-        for service in "''${services[@]}"; do
-            systemctl start "$service"
-        done
-
-    fi
-  '';
-in
 {
+  specialisation = {
+    "vfio".configuration = {
+      boot = {
+        extraModprobeConfig = ''
+          softdep nvidia pre: vfio-pci
+          options vfio-pci ids=10de:249d,10de:228b
+          options kvmfr static_size_mb=128
+          options kvm ignore_msrs=1 report_ignored_msrs=0 halt_poll_ns=0
+          options vfio-iommu-type1 allow_unsafe_interrupts=Y
+        '';
+        extraModulePackages = with config.boot.kernelPackages; [ kvmfr ];
+        kernelModules = [ "kvmfr" ]; # vfio will be loaded by libvirt
+        kernelParams = [ "intel_iommu=on" ];
+      };
+
+      # systemd.tmpfiles.rules = [
+      #   "f /dev/shm/looking-glass 0660 root kvm -"
+      # ];
+      services.udev.extraRules = ''
+        SUBSYSTEM=="kvmfr", OWNER="root", GROUP="kvm", MODE="0660"
+      '';
+
+      virtualisation.libvirtd.qemu.verbatimConfig = ''
+        namespaces = []
+        cgroup_device_acl = [
+          "/dev/null", "/dev/full", "/dev/zero",
+          "/dev/random", "/dev/urandom", "/dev/ptmx",
+          "/dev/kvm", "/dev/rtc", "/dev/hpet",
+          "/dev/kvmfr0"
+        ]
+      '';
+    };
+  };
+
   virtualisation = {
     containers = {
       enable = true;
@@ -105,8 +82,6 @@ in
               if [ "$1" == "win-vfio" ]; then
                 if [ "$2" == "prepare" ]; then
 
-                    ${lib.getExe switch-vfio} bind
-
                     echo 3 > /proc/sys/vm/drop_caches
                     echo 1 > /proc/sys/vm/compact_memory
 
@@ -134,13 +109,12 @@ in
 
                 elif [ "$2" == "release" ]; then
 
-                    ${lib.getExe switch-vfio} unbind
-
                     echo 0 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 
                     systemctl set-property --runtime -- user.slice AllowedCPUs=$SYS_TOTAL_CPUS
                     systemctl set-property --runtime -- system.slice AllowedCPUs=$SYS_TOTAL_CPUS
                     systemctl set-property --runtime -- init.scope AllowedCPUs=$SYS_TOTAL_CPUS
+
                 fi
               fi
             ''
@@ -151,35 +125,4 @@ in
   };
 
   programs.virt-manager.enable = true;
-
-  boot.kernelParams = [ "intel_iommu=on" ];
-
-  environment.systemPackages = [ switch-vfio ];
-
-  boot = {
-    kernelModules = [ "kvmfr" ];
-    extraModulePackages = with config.boot.kernelPackages; [ kvmfr ];
-    extraModprobeConfig = ''
-      options kvmfr static_size_mb=128
-      options kvm ignore_msrs=1 report_ignored_msrs=0 halt_poll_ns=0
-      options vfio-iommu-type1 allow_unsafe_interrupts=Y
-    '';
-  };
-
-  # systemd.tmpfiles.rules = [
-  #   "f /dev/shm/looking-glass 0660 root kvm -"
-  # ];
-  services.udev.extraRules = ''
-    SUBSYSTEM=="kvmfr", OWNER="root", GROUP="kvm", MODE="0660"
-  '';
-
-  virtualisation.libvirtd.qemu.verbatimConfig = ''
-    namespaces = []
-    cgroup_device_acl = [
-      "/dev/null", "/dev/full", "/dev/zero",
-      "/dev/random", "/dev/urandom", "/dev/ptmx",
-      "/dev/kvm", "/dev/rtc", "/dev/hpet",
-      "/dev/kvmfr0"
-    ]
-  '';
 }
