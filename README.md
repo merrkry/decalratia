f85042440b289ec767a79965d6e245519e03efec
merrkry <merrkry@tsubasa.moe>
2025-01-11 19:46:20 +0100

    ci: migrate to self-host cache

--- README.md
+++ README.md
@@ -14,11 +14,3 @@ All configuration in one place, powered by Nix.
 - `sapphire`: storage VPS
 
 More machines to be migrated to this flake.
-
-## CI
-
-Thanks Garnix for its generous free plan.
-
-Garnix CI will evaluate, build and cache updates of the repo. An Github Action is also deployed, to periodically bump flake inputs automatically if evaluation and builds are successful.
-
-As Garnix's public cache doesn't require any kind of authentication by default, an attacker may be able to get private flake inputs from it, using NAR hash provided in `flake.lock`. That's why this repo is published as a mirrow, with `flake.lock` completed wiped from git history.
--- profiles/base/nixd.nix
+++ profiles/base/nixd.nix
@@ -47,14 +47,8 @@ in
                 # "@wheel"
                 "remote-deployer"
               ];
-              substituters = [
-                "https://nix-community.cachix.org"
-                "https://cache.garnix.io"
-              ];
-              trusted-public-keys = [
-                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-              ];
+              substituters = [ "https://nix-cache.merrkry.com/s3" ];
+              trusted-public-keys = [ "s3:nWGYPAetx+CSk8VZCxb7mXQpK1ue6com52oMSPFxUN0=" ];
               flake-registry = "";
               nix-path = config.nix.nixPath;
             };
