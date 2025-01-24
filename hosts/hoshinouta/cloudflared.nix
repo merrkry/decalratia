233bc4e4bae1b5f6e1eaf544cc83f75132794a10
merrkry <merrkry@tsubasa.moe>
2025-01-24 18:50:00 +0100

    hoshinouta/cloudflared: manage via web dashboard

--- hosts/hoshinouta/cloudflared.nix
+++ hosts/hoshinouta/cloudflared.nix
@@ -7,30 +7,18 @@
 
   services.cloudflared = {
     enable = true;
-    # TODO: manage via web dashboard directly
+    # Managed viad web dashboard
+    # It seems unnecessary to set default ingress field, but eval will fail without it
     tunnels = {
       # hoshinouta0 tsubasa.moe
       "2e3988cf-3e46-43e9-a981-65af2306c5a3" = {
         credentialsFile = config.sops.secrets."cloudflared/hoshinouta0".path;
         default = "http_status:404";
-
-        ingress = {
-          "matrix.tsubasa.moe" = "http://127.0.0.1:443";
-          "m.tsubasa.moe" = "http://127.0.0.1:443";
-        };
       };
-
       # hoshinouta1 merrkry.com
       "2541af10-f0d8-4418-b88a-0b90db81dcf3" = {
         credentialsFile = config.sops.secrets."cloudflared/hoshinouta1".path;
         default = "http_status:404";
-
-        ingress = {
-          "memos.merrkry.com" = "http://127.0.0.1:${toString config.lib.ports.memos}";
-          "miniflux.merrkry.com" = "http://127.0.0.1:${toString config.lib.ports.miniflux}";
-          "rsshub.merrkry.com" = "http://127.0.0.1:${toString config.lib.ports.rsshub}";
-          "vaultwarden.merrkry.com" = "http://127.0.0.1:${toString config.lib.ports.vaultwarden}";
-        };
       };
     };
   };
