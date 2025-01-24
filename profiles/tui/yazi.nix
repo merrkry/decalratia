972a2939ff371e6bea48e367a63c07ffc1adfcb1
merrkry <merrkry@tsubasa.moe>
2025-01-24 22:41:45 +0100

    yazi: disable preload and preview on remote fs

--- profiles/tui/yazi.nix
+++ profiles/tui/yazi.nix
@@ -16,42 +16,60 @@ in
 
   config = lib.mkIf cfg.enable {
 
-    home-manager.users.${user} = {
+    home-manager.users.${user} =
+      let
+        hmConfig = config.home-manager.users.${user};
+      in
+      {
 
-      # https://yazi-rs.github.io/docs/installation
-      home.packages =
-        with pkgs;
-        (
-          [
-            p7zip-rar
-            jq
-            fd
-            ripgrep
-            fzf
-            zoxide
-          ]
-          ++ (lib.optionals cfg.enableDesktopUtils [
-            ffmpeg
-            poppler
-            imagemagick
-            wl-clipboard
-          ])
-        );
+        # https://yazi-rs.github.io/docs/installation
+        home.packages =
+          with pkgs;
+          (
+            [
+              p7zip-rar
+              jq
+              fd
+              ripgrep
+              fzf
+              zoxide
+            ]
+            ++ (lib.optionals cfg.enableDesktopUtils [
+              ffmpeg
+              poppler
+              imagemagick
+              wl-clipboard
+            ])
+          );
 
-      programs.yazi = {
-        enable = true;
-        settings = {
-          manager = {
-            ratio = [
-              0
-              4
-              6
+        programs.yazi = {
+          enable = true;
+          settings = {
+            manager = {
+              ratio = [
+                0
+                4
+                6
+              ];
+            };
+            # https://yazi-rs.github.io/docs/tips/#folder-previewer
+            # How to scale this?
+            plugin.prepend_preloaders = [
+              {
+                name = "${hmConfig.home.homeDirectory}/Documents/Nextcloud/**";
+                run = "noop";
+              }
+            ];
+            plugin.prepend_previewers = [
+              {
+                name = "${hmConfig.home.homeDirectory}/Documents/Nextcloud/**";
+                run = "noop";
+              }
             ];
           };
         };
-      };
 
-    };
+      };
 
   };
 }
