50ddda6ebec491d684123579bb76e53eec2d5f1a
merrkry <merrkry@tsubasa.moe>
2025-02-08 16:20:20 +0100

    perimadeia: add substituters

--- hosts/perimadeia/atticd.nix
+++ hosts/perimadeia/atticd.nix
@@ -5,6 +5,17 @@ let
   port = 8080;
 in
 {
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.garnix.io"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
+
   security = {
     # Required for atticd-atticadm to work
     polkit.enable = true;
