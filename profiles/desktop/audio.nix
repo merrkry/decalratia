de4287dcdddbc8264cdf4a9d49be3ec050c6b71a
merrkry <merrkry@tsubasa.moe>
2025-02-11 15:22:21 +0100

    desktop: port more cachyos tweaks

--- profiles/desktop/audio.nix
+++ profiles/desktop/audio.nix
@@ -61,6 +61,11 @@ in
       };
     };
 
+    systemd.tmpfiles.rules = [
+      "w! /sys/class/rtc/rtc0/max_user_freq - - - - 3072"
+      "w! /proc/sys/dev/hpet/max-user-freq  - - - - 3072"
+    ];
+
     users.users.${user}.extraGroups = [ "audio" ];
 
     home-manager.users.${user} = {
