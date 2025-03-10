3eb1b14a5b7eec999bd98fb5a35662a9c27e1526
merrkry <merrkry@tsubasa.moe>
2025-03-10 23:49:29 +0800

    chromium: enable hardware acceleration

--- profiles/gui/chromium.nix
+++ profiles/gui/chromium.nix
@@ -21,13 +21,19 @@ in
         enable = true;
         # https://github.com/ungoogled-software/ungoogled-chromium/issues/3226
         package = pkgs.chromium.override { enableWideVine = true; };
-        commandLineArgs = lib.chromiumArgs ++ [ "--password-store=gnome-libsecret" ];
-        extensions = [
-          {
-            id = "ocaahdebbfolfmndjeplogmgcagdmblk";
-            updateUrl = "https://raw.githubusercontent.com/NeverDecaf/chromium-web-store/refs/heads/master/updates.xml";
-          }
+        commandLineArgs = lib.chromiumArgs ++ [
+          "--password-store=gnome-libsecret"
+          "--enable-features=AcceleratedVideoDecodeLinuxGL"
+          "--ignore-gpu-blocklist"
+          "--enable-zero-copy"
         ];
+        # doesn't work for ungoogled-chromium
+        # extensions = [
+        #   {
+        #     id = "ocaahdebbfolfmndjeplogmgcagdmblk";
+        #     updateUrl = "https://raw.githubusercontent.com/NeverDecaf/chromium-web-store/refs/heads/master/updates.xml";
+        #   }
+        # ];
       };
 
     };
