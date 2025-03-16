e4efde60cea550fafea8e9aa8970c0eb334c17e6
merrkry <merrkry@tsubasa.moe>
2025-03-16 14:50:44 +0800

    mautrix-telegram: bump to git

--- overlays/modifications.nix
+++ overlays/modifications.nix
@@ -1,5 +1,16 @@
 { pkgs, ... }:
 {
+  mautrix-telegram = pkgs.mautrix-telegram.overrideAttrs (
+    finalAttrs: oldAttrs: {
+      src = pkgs.fetchFromGitHub {
+        owner = "mautrix";
+        repo = "telegram";
+        rev = "caefda582b0ba43f53cb66e5c0dffc65ab22b890";
+        hash = "sha256-j8l8ziJCYK4NlimDJeVxQORPT/X91pwBHN6K0S1tAXQ=";
+      };
+    }
+  );
+
   # Allow to keep the same name between updates
   # https://github.com/chaotic-cx/nyx/blob/main/pkgs/proton-ge-custom/default.nix
   # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/pr/proton-ge-bin/package.nix
