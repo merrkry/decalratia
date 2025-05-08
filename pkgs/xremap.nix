3a5f5e8e59c57e7101d0fdf19f3cfb4e7980310d
merrkry <merrkry@tsubasa.moe>
2025-05-08 16:12:46 +0200

    xremap: 0.10.10 -> 0.10.11

--- pkgs/xremap.nix
+++ pkgs/xremap.nix
@@ -5,17 +5,17 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "xremap";
-  version = "0.10.10";
+  version = "0.10.11";
 
   src = fetchFromGitHub {
     owner = "xremap";
     repo = "xremap";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-U2TSx0O2T53lUiJNpSCUyvkG4Awa0+a4N6meFn09LbI";
+    hash = "sha256-z9QgiwG4muKYc1N9ycjs9r9QXB8JvzTdkCxu2c3mB9o=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-8IVexszltzlBBShGkjZwyDKs02udrVpZEOwfzRzAraU";
+  cargoHash = "sha256-X2tcyf+vG6VFscInyDpcfZr79mSF+M9ziA6/cMJCL7w=";
 
   meta = {
     description = "Key remapper for X11 and Wayland";
