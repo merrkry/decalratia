4b4f666e3e8bf048b0f7cbb650ff36597322ab8a
merrkry <merrkry@tsubasa.moe>
2025-06-06 16:19:29 +0200

    kitty: cleanup keybinds

--- profiles/gui/kitty.nix
+++ profiles/gui/kitty.nix
@@ -18,6 +18,21 @@ in
         enable = true;
         settings = {
           enable_audio_bell = "no";
+          clear_all_shortcuts = "yes";
+        };
+        # https://sw.kovidgoyal.net/kitty/conf/#keyboard-shortcuts
+        keybindings = {
+          "ctrl+shift+c" = "copy_to_clipboard";
+          "ctrl+shift+v" = "paste_from_clipboard";
+          "ctrl+shift+up" = "scroll_line_up";
+          "ctrl+shift+k" = "scroll_line_up";
+          "ctrl+shift+down" = "scroll_line_down";
+          "ctrl+shift+j" = "scroll_line_down";
+          "ctrl+shift+page_up" = "scroll_page_up";
+          "ctrl+shift+page_down" = "scroll_page_down";
+          "ctrl+shift+equal" = "change_font_size all +2.0";
+          "ctrl+shift+minus" = "change_font_size all -2.0";
+          "ctrl+shift+backspace" = "change_font_size all 0";
         };
       };
 
