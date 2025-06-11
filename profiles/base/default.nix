e24bbc8ee486123ebf8f95638a8a05674d875e0a
merrkry <merrkry@tsubasa.moe>
2025-06-11 16:22:12 +0200

    systemd: refactor ksm config

--- profiles/base/default.nix
+++ profiles/base/default.nix
@@ -12,65 +12,82 @@ in
 
   options.profiles.base = {
     enable = lib.mkEnableOption "base profile";
+    enableGlobalKSM = lib.mkEnableOption "global KSM";
   };
 
-  config = lib.mkIf cfg.enable {
-
-    boot = {
-      tmp.cleanOnBoot = true;
-      initrd.systemd.enable = true;
-    };
-
-    # workarounds for rootless podman subuid/subgid
-    # only works for main user
-    # more see https://github.com/linyinfeng/dotfiles/blob/main/nixos/modules/users/userborn-extra.nix
-    # and https://github.com/nikstur/userborn/issues/7
-    environment.etc =
-      let
-        autosubs = {
-          text = ''
-            ${user}:100000:65536
-          '';
-          mode = "0444";
-        };
-      in
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
       {
-        "subgid" = autosubs;
-        "subuid" = autosubs;
-      };
+        boot = {
+          tmp.cleanOnBoot = true;
+          initrd.systemd.enable = true;
+        };
 
-    security.sudo.extraConfig = ''
-      Defaults lecture="never"
-    '';
+        # workarounds for rootless podman subuid/subgid
+        # only works for main user
+        # more see https://github.com/linyinfeng/dotfiles/blob/main/nixos/modules/users/userborn-extra.nix
+        # and https://github.com/nikstur/userborn/issues/7
+        environment.etc =
+          let
+            autosubs = {
+              text = ''
+                ${user}:100000:65536
+              '';
+              mode = "0444";
+            };
+          in
+          {
+            "subgid" = autosubs;
+            "subuid" = autosubs;
+          };
 
-    # IMPORTANT: this is broken on first-time deployment! Install the system with this disabled, and then enable this via rebuild.
-    # currently breaks rootless podman due to lack of subuid and subgid support,
-    # see https://github.com/nikstur/userborn/issues/7
-    services.userborn = {
-      enable = true;
-      passwordFilesLocation = "/var/lib/nixos";
-    };
+        security.sudo.extraConfig = ''
+          Defaults lecture="never"
+        '';
 
-    systemd = {
-      extraConfig = ''
-        DefaultLimitNOFILE=2048:2097152
-      '';
+        # IMPORTANT: this is broken on first-time deployment! Install the system with this disabled, and then enable this via rebuild.
+        # currently breaks rootless podman due to lack of subuid and subgid support,
+        # see https://github.com/nikstur/userborn/issues/7
+        services.userborn = {
+          enable = true;
+          passwordFilesLocation = "/var/lib/nixos";
+        };
 
-      tmpfiles.rules = [ "w! /sys/kernel/mm/ksm/run - - - - 1" ];
+        systemd = {
+          extraConfig = ''
+            DefaultLimitNOFILE=2048:2097152
+          '';
+          user.extraConfig = ''
+            DefaultLimitNOFILE=1024:1048576
+          '';
+        };
 
-      user.extraConfig = ''
-        DefaultLimitNOFILE=1024:1048576
-      '';
-    };
+        users.mutableUsers = false;
 
-    users.mutableUsers = false;
+        home-manager.users.${user} = {
+          home.stateVersion = config.system.stateVersion;
+          systemd.user.startServices = "sd-switch";
+        };
+      }
+      (lib.mkIf cfg.enableGlobalKSM {
+        # https://github.com/CachyOS/CachyOS-PKGBUILDS/tree/master/cachyos-ksm-settings
+        systemd = {
+          services =
+            let
+              ksmConfig = {
+                MemoryKSM = "yes";
+              };
+            in
+            {
+              "getty@".serviceConfig = ksmConfig;
+              "user@".serviceConfig = ksmConfig;
+              # display managers
+              "greetd".serviceConfig = ksmConfig;
+            };
 
-    home-manager.users.${user} = {
-
-      home.stateVersion = config.system.stateVersion;
-      systemd.user.startServices = "sd-switch";
-
-    };
-
-  };
+          tmpfiles.rules = [ "w! /sys/kernel/mm/ksm/run - - - - 1" ];
+        };
+      })
+    ]
+  );
 }
