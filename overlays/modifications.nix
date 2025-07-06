bbf7093b83ceb14ff9a9113a24ac8cc4b6ed13cc
merrkry <merrkry@tsubasa.moe>
2025-07-06 15:05:22 +0200

    materialgram: wrap QT_QPA_PLATFORMTHEME

--- overlays/modifications.nix
+++ overlays/modifications.nix
@@ -1,4 +1,14 @@
 { pkgs, ... }:
 {
-
+  # Use system file chooser provided by xdg portal instead of bundled ugly qt file chooser
+  materialgram = pkgs.materialgram.overrideAttrs (
+    final: prev: {
+      qtWrapperArgs = prev.qtWrapperArgs ++ [
+        "--prefix"
+        "QT_QPA_PLATFORMTHEME"
+        ":"
+        "gtk3"
+      ];
+    }
+  );
 }
