84e779e9fac60f1a9b89a16e62375d29242e2294
merrkry <merrkry@tsubasa.moe>
2025-07-18 22:45:40 +0200

    tmux: split config, ricing

--- profiles/tui/tmux/default.nix
+++ profiles/tui/tmux/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   user,
   ...
 }:
@@ -14,25 +15,21 @@ in
 
   config = lib.mkIf cfg.enable {
     home-manager.users.${user} = {
-      # imported by stylix
-      # https://github.com/tinted-theming/tinted-tmux
-      home.sessionVariables = {
-        TINTED_TMUX_OPTION_STATUSBAR = 1;
-      };
-
       programs.tmux = {
         enable = true;
+        plugins = with pkgs.tmuxPlugins; [ vim-tmux-navigator ];
+
+        # programs.tmux explicitly set these option by default
+        aggressiveResize = true;
         baseIndex = 1;
         clock24 = true;
-        escapeTime = 10;
+        focusEvents = true;
         keyMode = "vi";
         mouse = true;
         terminal = "tmux-256color";
-        # about -g/-s, see https://github.com/tmux/tmux/wiki/Getting-Started#changing-options
+
         extraConfig = ''
-          set -g renumber-windows on
-          set -g set-clipboard on
-          set -ga terminal-overrides ",xterm-256color:Tc,foot:Tc,xterm-kitty:Tc"
+          source-file ${./tmux.conf}
         '';
       };
 
