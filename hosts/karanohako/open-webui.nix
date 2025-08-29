8b71ca44cd326a37264ffa982dd054946a3ddb31
merrkry <merrkry@tsubasa.moe>
2025-08-29 19:27:53 +0200

    open-webui: deploy with container

--- hosts/karanohako/open-webui.nix
+++ hosts/karanohako/open-webui.nix
@@ -8,42 +8,28 @@
 let
   serviceName = "open-webui";
   domainName = "llm.tsubasa.moe";
+  workDir = "/var/lib/${serviceName}";
+  port = helpers.servicePorts.${serviceName};
 in
 {
+  networking.firewall.interfaces."podman0".allowedTCPPorts = [ 5432 ];
+
   services = {
     nginx.virtualHosts.${domainName} = {
       forceSSL = true;
       useACMEHost = "karanohako.tsubasa.moe";
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.open-webui.port}";
+        proxyPass = "http://127.0.0.1:${toString port}";
         proxyWebsockets = true;
       };
     };
 
-    open-webui = {
-      enable = true;
-      environment = {
-        # We need this to let environment variable overwrite persisted config in db.
-        # According to code, if this is set to false while related env var is not set,
-        # open-webui will use default value instead of read from db.
-        # Not so sure if there's any thing we want to keep, so better keep this unset unless necessary.
-        # https://github.com/open-webui/open-webui/blob/63256136ef8322210c01c2bb322097d1ccfb8c6f/backend/open_webui/config.py#L163-L174
-        # ENABLE_PERSISTENT_CONFIG = "False";
-
-        WEBUI_URL = "https://${domainName}";
-        ENABLE_LOGIN_FORM = "False";
-        VECTOR_DB = "pgvector";
-        ENABLE_OAUTH_SIGNUP = "True";
-        OAUTH_MERGE_ACCOUNTS_BY_EMAIL = "True";
-        OAUTH_CLIENT_ID = "open-webui";
-        OPENID_PROVIDER_URL = "https://id.tsubasa.moe/oauth2/openid/${serviceName}/.well-known/openid-configuration";
-        DATABASE_URL = "postgres:///${serviceName}?host=/run/postgresql";
-      };
-      environmentFile = config.sops.secrets."open-webui".path;
-      port = helpers.servicePorts.open-webui;
-    };
     postgresql = {
       enable = true;
+      enableTCPIP = true;
+      authentication = ''
+        host open-webui open-webui 10.88.0.0/16 md5
+      '';
       ensureUsers = [
         {
           name = serviceName;
@@ -60,10 +46,12 @@ in
   };
 
   systemd.services = {
-    ${serviceName} = {
-      after = [ "postgresql.service" ];
+    ${config.virtualisation.oci-containers.containers.${serviceName}.serviceName} = {
+      after = [
+        "postgresql.service"
+        "sops-install-secrets.service"
+      ];
       requires = [ "postgresql.service" ];
-      path = [ pkgs.ffmpeg ];
     };
 
     postgresql.serviceConfig.ExecStartPost =
@@ -77,5 +65,40 @@ in
           ${lib.getExe' config.services.postgresql.package "psql"} -d "${serviceName}" -f "${sqlFile}"
         ''
       ];
+  };
+
+  virtualisation.oci-containers.containers.${serviceName} = {
+    image = "ghcr.io/open-webui/open-webui:main";
+    pull = "newer";
+
+    volumes = [ "open-webui:/app/backend/data" ];
+    ports = [ "127.0.0.1:${toString port}:8080" ];
+    extraOptions = [
+      "--add-host=host.docker.internal:host-gateway"
+    ];
+
+    environment = {
+      # sync with services.open-webui for compatibility
+      STATIC_DIR = "${workDir}/static";
+      DATA_DIR = "${workDir}/data";
+      HF_HOME = "${workDir}/hf_home";
+      SENTENCE_TRANSFORMERS_HOME = "${workDir}/transformers_home";
+
+      # We need this to let environment variable overwrite persisted config in db.
+      # According to code, if this is set to false while related env var is not set,
+      # open-webui will use default value instead of read from db.
+      # Not so sure if there's any thing we want to keep, so better keep this unset unless necessary.
+      # https://github.com/open-webui/open-webui/blob/63256136ef8322210c01c2bb322097d1ccfb8c6f/backend/open_webui/config.py#L163-L174
+      # ENABLE_PERSISTENT_CONFIG = "False";
+
+      WEBUI_URL = "https://${domainName}";
+      ENABLE_LOGIN_FORM = "False";
+      VECTOR_DB = "pgvector";
+      ENABLE_OAUTH_SIGNUP = "True";
+      OAUTH_MERGE_ACCOUNTS_BY_EMAIL = "True";
+      OAUTH_CLIENT_ID = "open-webui";
+      OPENID_PROVIDER_URL = "https://id.tsubasa.moe/oauth2/openid/${serviceName}/.well-known/openid-configuration";
+    };
+    environmentFiles = [ config.sops.secrets."open-webui".path ];
   };
 }
