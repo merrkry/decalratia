0bea3fad3c08052844ad02a3b467985f0cfacdc8
Merrkry <merrkry@tsubasa.moe>
2025-11-02 01:55:05 +0100

    pkgs: remove packages maintained in my NUR

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,4 @@ pkgs: {
   apple-color-emoji = pkgs.callPackage ./apple-color-emoji.nix { };
   apple-system-fonts = pkgs.callPackage ./apple-system-fonts.nix { };
   bookerly = pkgs.callPackage ./bookerly.nix { };
-  deeplx = pkgs.callPackage ./deeplx.nix { };
-  kvlibadwaita-kvantum = pkgs.callPackage ./kvlibadwaita-kvantum.nix { };
 }
