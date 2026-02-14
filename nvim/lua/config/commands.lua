4143095eadbd78005d5530e231c4c9745c1ba60f
Merrkry <merrkry@tsubasa.moe>
2026-02-14 22:41:35 +0100

    feat(nvim): clean nvim.lsp.hover_range with cmd

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -1,7 +1,13 @@
 vim.api.nvim_create_user_command("RestoreStyling", function()
-	local ns_id = vim.api.nvim_get_namespaces()["nvim.lsp.references_some"] ---@as integer | nil
-	if ns_id then
-		vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+	local extmarks = {
+		"nvim.lsp.hover_range",
+		"nvim.lsp.references_some",
+	}
+	for _, extmark in ipairs(extmarks) do
+		local ns_id = vim.api.nvim_get_namespaces()[extmark] ---@as integer | nil
+		if ns_id then
+			vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+		end
 	end
 
 	if vim.snippet.active() then
