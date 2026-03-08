0fb7d36b97788fc7c76ce8e44381fda6cc3c0123
Merrkry <merrkry@tsubasa.moe>
2026-03-08 15:48:53 +0800

    osnium: document workarounds

--- hosts/osmium/hardware.nix
+++ hosts/osmium/hardware.nix
@@ -41,9 +41,22 @@ in
     };
     kernelModules = [ "kvm-intel" ];
     kernelParams = [
+      # TODO: try remove with Linux 7.0
+      # https://gitlab.freedesktop.org/drm/xe/kernel/-/issues/7284
       "xe.enable_psr=0"
       "xe.enable_panel_replay=0"
     ];
+  };
+
+  # TODO: remove with libinput 1.31
+  # https://www.bilibili.com/read/cv42222859
+  environment.etc."libinput/local-overrides.quirks".text = lib.generators.toINI { } {
+    "Lenovo ThinkBook 16 G8+ IPH Touchpad" = {
+      MatchName = "*GXTP5100*";
+      MatchDMIModalias = "dmi:bvnLENOVO:bvrSWCN15WW:bd12/17/2025:br1.15:efr1.15:svnLENOVO:pn21VG:pvrThinkBook14G8+IPH:rvnLENOVO:rnLNVNB161216:rvrSDK0T76577WIN:cvnLENOVO:ct10:cvrThinkBook14G8+IPH:skuLENOVO_MT_21VG_BU_idea_FM_ThinkBook14G8+IPH:";
+      MatchUdevType = "touchpad";
+      ModelPressurePad = 1;
+    };
   };
 
   fileSystems = {
